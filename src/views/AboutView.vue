dfa010e9b7bdb55e4e17a93dac6fd327985f3f59
Juan Pablo Silva <juan.silva@uniacc.cl>
2024-12-19 12:16:53 -0300

    INICIO

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>DE NUEVO</h1>
+    <p> acá va a ir la información</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
